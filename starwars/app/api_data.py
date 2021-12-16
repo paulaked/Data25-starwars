ad31e8b19e3eb37ad47cd6fdda16534660a6912b
michael mcarthur <mmcarthur@spartaglobal.com>
2021-12-16 17:28:31 +0000

    function for getting api urls

--- starwars/app/api_data.py
+++ starwars/app/api_data.py
@@ -3,22 +3,16 @@ import json
 from pprint import pprint
 
 
-starwars = requests.get("https://www.swapi.tech/api/starships/")
+def get_api_url():
+    starwars = requests.get("https://www.swapi.tech/api/starships/")
+    total_records = str(starwars.json()['total_records'])
+    starwars = requests.get("https://www.swapi.tech/api/starships?page=1&limit=" + total_records)
 
-total_records = str(starwars.json()['total_records'])
+    url_results_list = []
 
-starwars = requests.get("https://www.swapi.tech/api/starships?page=1&limit=" + total_records)
+    for i in starwars.json()['results']:
+        url_results_list.append(i['url'])
 
-url_results_list = []
+    print(url_results_list)
 
-for i in starwars.json()['results']:
-    url_results_list.append(i['url'])
-
-print(url_results_list)
-
-# for i in a['results']
-# for i in star_wars.content['results']:
-#     print(i)
-# print(star_wars.content)
-
-# json_file = json.dumps()
+get_api_url()

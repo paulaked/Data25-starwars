9449da1d6a8207f989430183da09e0ff3a7fde06
William Chambers <williamchambers@Williams-MacBook-Air.local>
2021-12-16 17:50:14 +0000

    API refactored

--- starwars/app/API.py
+++ starwars/app/API.py
@@ -2,15 +2,22 @@ import requests
 import json
 from pprint import pprint
 
-starwars = requests.get("https://www.swapi.tech/api/starships/")
+def get_api_url():
+    starwars = requests.get("https://www.swapi.tech/api/starships/")
+    total_records = str(starwars.json()['total_records'])
+    starwars= requests.get("https://www.swapi.tech/api/starships?page=1&limit=" + total_records)
 
-total_records = str(starwars.json()['total_records'])
+    url_results = []
 
-starwars= requests.get("https://www.swapi.tech/api/starships?page=1&limit=" + total_records)
+    for i in starwars.json()['results']:
+        url_results.append(i['url'])
 
-url_results_list = []
+    return url_results
 
-for i in starwars.json()['results']:
-    url_results_list.append(i['url'])
+print(get_api_url())
 
-print(url_results_list)
+def create_json(url_list):
+    url_list = get_api_url()
+    return url_list
+
+print(create_json())

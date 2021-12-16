b757e760d64fde71cbfb01ea66857a7893895c18
Zoe <zmoakhar@spartaglobal.com>
2021-12-16 17:47:29 +0000

    listed the starship urls as a python list

--- pilotzm/main.py
+++ pilotzm/main.py
@@ -4,12 +4,25 @@ from pprint import pprint
 response = requests.get('https://www.swapi.tech/api/starships')
 print(response.status_code)
 ship = response.json()
-pprint(ship)
+#pprint(ship)
 my_list = []
 for val in ship['results']:
     for value in val.values():
         my_list.append(value)
-        print(len(my_list))
+
+#print(my_list)
+url_list = []
+for i in my_list:
+    if i[0] == 'h':
+        url_list.append(i)
+
+print(url_list)
+
+
+
+
+        #my_list.append(value)
+        #print(len(my_list))
 
 #print(len(my_list))
     #print(val, type(val))

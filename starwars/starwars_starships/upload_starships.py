05d6eeecae1dfec1d174616226f8871d512f39c9
Your Name <you@example.com>
2021-12-16 14:17:22 +0000

    Starship APIs uploaded

---
+++ starwars/starwars_starships/upload_starships.py
@@ -0,0 +1,12 @@
+import requests
+
+starships_id2 = requests.get("https://www.swapi.tech/api/starships/2")
+starships_id3 = requests.get("https://www.swapi.tech/api/starships/3")
+starships_id5 = requests.get("https://www.swapi.tech/api/starships/5")
+starships_id9 = requests.get("https://www.swapi.tech/api/starships/9")
+starships_id10 = requests.get("https://www.swapi.tech/api/starships/10")
+starships_id11 = requests.get("https://www.swapi.tech/api/starships/11")
+starships_id12 = requests.get("https://www.swapi.tech/api/starships/12")
+starships_id13 = requests.get("https://www.swapi.tech/api/starships/13")
+starships_id15 = requests.get("https://www.swapi.tech/api/starships/15")
+starships_id17 = requests.get("https://www.swapi.tech/api/starships/17")

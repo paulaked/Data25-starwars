156d8d2e388fc3aa8aa8301ad89edf5e57c41ad2
SpartaBarnett <dbarnett@spartaglobal.com>
2021-12-16 18:43:40 +0000

    added comments to functions file in order to give further explanation

--- starwars/app/functions.py
+++ starwars/app/functions.py
@@ -3,17 +3,15 @@ import requests
 import json
 import pymongo
 import os
-if __name__ == '__main__':
-    pass
 
 
-def get_from_api(): #collects information
+def get_from_api():     # fetches data about the starships from teh API
     responses = []
     response = requests.get('https://www.swapi.tech/api/starships/').json()
     responses += response['results']
-    while response['next'] is not None:
-        response = requests.get(response['next']).json()
-        responses += response['results']
+    while response['next'] is not None:     # ensures that all data is collected
+        response = requests.get(response['next']).json()    # collects the link to the next page
+        responses += response['results']    # adds information about the starships to the responses list
     return responses
 
 
@@ -21,6 +19,7 @@ def collect_starships():
     starships = []
     for starship in get_from_api():
         starships.append(requests.get(starship['url']).json()['result']['properties'])
+        # uses the API to collect additional data
     return starships
 
 
@@ -30,7 +29,8 @@ def collect_pilots():
         pilots = []
         for pilot in starship['pilots']:
             pilots.append(requests.get(pilot).json()['result']['properties']['name'])
-        starship.update({'pilots': pilots})
+            # collects the name for all listed pilots
+        starship.update({'pilots': pilots})  # replaces the pilot list with a list of character names
         starships.append(starship)
     return starships
 
@@ -43,7 +43,7 @@ def store_data():
     for starship in collect_pilots():
         file_name = os.path.join('starships', str(starship['name']) + '.json')
         with open(file_name, 'w') as json_file:
-            json.dump(starship, json_file)
+            json.dump(starship, json_file)  # stores the data for all starships into an individual JSON file
 
 
 client = pymongo.MongoClient()
@@ -52,12 +52,12 @@ db = client['starwars']
 
 def load_data():
     db['starships'].drop()
-    starships = db['starships']
+    starships = db['starships']  # drops the collection 'starships' then creates it again
     for file in os.listdir('starships'):
         file_name = os.path.join('starships', file)
         with open(file_name, 'r') as json_file:
             starship = json.load(json_file)
-        db.starships.insert_one(starship)
+        db.starships.insert_one(starship)    # loads the data into the starship collection in MongoDB
 
 
 def references_pilot():
@@ -66,10 +66,9 @@ def references_pilot():
         pilots = [{}]
         for pilot in starship['pilots']:
             key = db.characters.find_one({'name': pilot})['_id']
+            # collects the object id for the pilots based on their name
             reference = {'_id': key}
             pilots.append(reference)
         db.starships.update_one({'name': starship['name']}, {'$set': {'pilots': pilots}})
+        # replaces the pilot field with a document containing the pilot's object id for every pilot
 
-
-for i in db.starships.find({}):
-    print(type(i))

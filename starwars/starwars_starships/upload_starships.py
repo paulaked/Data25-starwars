93bb4249943bf3809e43da00559a9eec7581d139
Your Name <you@example.com>
2021-12-16 16:07:10 +0000

    function created for uploading starships

--- starwars/starwars_starships/upload_starships.py
+++ starwars/starwars_starships/upload_starships.py
@@ -1,24 +1,39 @@
 import requests
 import pprint as pp
 
-starships_id2 = requests.get("https://www.swapi.tech/api/starships/2")
-starships_id3 = requests.get("https://www.swapi.tech/api/starships/3")
-starships_id5 = requests.get("https://www.swapi.tech/api/starships/5")
-starships_id9 = requests.get("https://www.swapi.tech/api/starships/9")
-starships_id10 = requests.get("https://www.swapi.tech/api/starships/10")
-starships_id11 = requests.get("https://www.swapi.tech/api/starships/11")
-starships_id12 = requests.get("https://www.swapi.tech/api/starships/12")
-starships_id13 = requests.get("https://www.swapi.tech/api/starships/13")
-starships_id15 = requests.get("https://www.swapi.tech/api/starships/15")
-starships_id17 = requests.get("https://www.swapi.tech/api/starships/17")
 
-ss_id2 = (starships_id2.json()['result']['properties'])
-ss_id3 = (starships_id3.json()['result']['properties'])
-ss_id5 = (starships_id5.json()['result']['properties'])
-ss_id9 = (starships_id9.json()['result']['properties'])
-ss_id10 = (starships_id10.json()['result']['properties'])
-ss_id11 = (starships_id11.json()['result']['properties'])
-ss_id12 = (starships_id12.json()['result']['properties'])
-ss_id13 = (starships_id13.json()['result']['properties'])
-ss_id15 = (starships_id15.json()['result']['properties'])
-ss_id17 = (starships_id17.json()['result']['properties'])
+# starships_id2 = requests.get("https://www.swapi.tech/api/starships/2")
+# # starships_id3 = requests.get("https://www.swapi.tech/api/starships/3")
+# # starships_id5 = requests.get("https://www.swapi.tech/api/starships/5")
+# # starships_id9 = requests.get("https://www.swapi.tech/api/starships/9")
+# # starships_id10 = requests.get("https://www.swapi.tech/api/starships/10")
+# # starships_id11 = requests.get("https://www.swapi.tech/api/starships/11")
+# # starships_id12 = requests.get("https://www.swapi.tech/api/starships/12")
+# # starships_id13 = requests.get("https://www.swapi.tech/api/starships/13")
+# # starships_id15 = requests.get("https://www.swapi.tech/api/starships/15")
+# # starships_id17 = requests.get("https://www.swapi.tech/api/starships/17")
+
+
+# ss_id2 = (starships_id2.json()['result']['properties'])
+# # ss_id3 = (starships_id3.json()['result']['properties'])
+# # ss_id5 = (starships_id5.json()['result']['properties'])
+# # ss_id9 = (starships_id9.json()['result']['properties'])
+# # ss_id10 = (starships_id10.json()['result']['properties'])
+# # ss_id11 = (starships_id11.json()['result']['properties'])
+# # ss_id12 = (starships_id12.json()['result']['properties'])
+# # ss_id13 = (starships_id13.json()['result']['properties'])
+# # ss_id15 = (starships_id15.json()['result']['properties'])
+# # ss_id17 = (starships_id17.json()['result']['properties'])
+
+def insert_starships(url_link):
+    initial_list = []
+    url_ending = [2, 3, 5, 9, 10, 11, 12, 13, 15, 17]
+
+    for n in url_ending:
+        new_starship = requests.get(url_link + str(n))
+        initial_list.append(new_starship.json()['result']['properties'])
+
+    return initial_list
+
+
+pp.pprint(insert_starships("https://www.swapi.tech/api/starships/"))

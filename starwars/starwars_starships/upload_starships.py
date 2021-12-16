bcd59e2754893c12fdacb4a8256bc08e0b0bded1
Your Name <you@example.com>
2021-12-16 14:24:43 +0000

    starship data stored as single object

--- starwars/starwars_starships/upload_starships.py
+++ starwars/starwars_starships/upload_starships.py
@@ -1,4 +1,5 @@
 import requests
+import pprint as pp
 
 starships_id2 = requests.get("https://www.swapi.tech/api/starships/2")
 starships_id3 = requests.get("https://www.swapi.tech/api/starships/3")
@@ -10,3 +11,14 @@ starships_id12 = requests.get("https://www.swapi.tech/api/starships/12")
 starships_id13 = requests.get("https://www.swapi.tech/api/starships/13")
 starships_id15 = requests.get("https://www.swapi.tech/api/starships/15")
 starships_id17 = requests.get("https://www.swapi.tech/api/starships/17")
+
+ss_id2 = (starships_id2.json()['result']['properties'])
+ss_id3 = (starships_id3.json()['result']['properties'])
+ss_id5 = (starships_id5.json()['result']['properties'])
+ss_id9 = (starships_id9.json()['result']['properties'])
+ss_id10 = (starships_id10.json()['result']['properties'])
+ss_id11 = (starships_id11.json()['result']['properties'])
+ss_id12 = (starships_id12.json()['result']['properties'])
+ss_id13 = (starships_id13.json()['result']['properties'])
+ss_id15 = (starships_id15.json()['result']['properties'])
+ss_id17 = (starships_id17.json()['result']['properties'])

15ebafe204d8a1b6152cbdeb210d27efb637914e
Alisha Parmar <alishaparmar@alishas-mbp.home>
2021-12-16 15:12:14 +0000

    import the api starships data

---
+++ main.py
@@ -0,0 +1,11 @@
+#import the starships data from the api
+import requests
+import pymongo
+from pprint import pprint
+
+starship_request = requests.get("https://www.swapi.tech/api/starships/")
+
+starships = starship_request.json()
+
+pprint(starships["pilots"])
+

6b53d932592afa6e9c3986af3a2ef77aeef37127
SpartaBarnett <dbarnett@spartaglobal.com>
2021-12-16 14:54:24 +0000

    added third test

--- starwars/tests/app_test.py
+++ starwars/tests/app_test.py
@@ -4,6 +4,8 @@ from app.__main__ import *
 def test_get_from_api():
     assert type(get_from_api()) is list
 
-
 def test_collect_starships():
     assert type(collect_starships()) is list
+
+def test_collect_pilots():
+    assert type(collect_pilots()) is list

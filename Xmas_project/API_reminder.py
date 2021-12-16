da12232aed9c9ffd284eefaaa08e33af656856d1
AdamMousley <AMousley@spartaglobal.com>
2021-12-16 17:46:53 +0000

    Accessed first 10 url from starships in pycharm

--- Xmas_project/API_reminder.py
+++ Xmas_project/API_reminder.py
@@ -3,18 +3,29 @@ import requests
 import json
 from pprint import pprint
 
-# starwars_people = requests.get("https://www.swapi.tech/api/people")
-# json_people = starwars_people.json()
-# pprint(star_wars_people.json())
+starwars_people = requests.get("https://www.swapi.tech/api/people")
+json_people = starwars_people.json()
+#pprint(starwars_people.json())
 
 raw_starships = requests.get("https://www.swapi.tech/api/starships")
 json_starships = raw_starships.json()
-pprint(json_starships)
 
-# pprint(ship.status_code) # Produces 200
+for i in json_starships["results"]:
+    print(i["url"])
 
-# with open("starships_API_URLs.json", "w") as jsonfile:
-#     json.dump(star_wars_ships, jsonfile)
+
+# for i in json_starships["results"]:
+#     if next != None:
+#         requests.get(i["next"])
+#     print(i['url'])
+
+# pprint(json_starships)
+# pprint(json_starship.status_code) # Produces 200
+
+# for starship in json_starships:
+#     print(starship["results"])
+    #pprint(json_starships[starship])
+
 
 # ships = []
 # for ship in star_wars_ships:

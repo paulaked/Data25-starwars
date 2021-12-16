4061789a3dfb0900e51bc46d65edf1612ef30c4e
SpartaBarnett <dbarnett@spartaglobal.com>
2021-12-16 14:28:11 +0000

    updated first function

--- starwars/app/__main__.py
+++ starwars/app/__main__.py
@@ -6,18 +6,12 @@ if __name__ == '__main__':
 
 
 def get_from_api():
-    response1 = requests.get('https://www.swapi.tech/api/starships/')
-    response2 = requests.get('https://www.swapi.tech/api/starships?page=2&limit=10')
-    response3 = requests.get('https://www.swapi.tech/api/starships?page=3&limit=10')
-    response4 = requests.get('https://www.swapi.tech/api/starships?page=4&limit=10')
+    responses = []
+    response = requests.get('https://www.swapi.tech/api/starships/').json()
+    responses += response['results']
+    while response['next'] is not None:
+        response = requests.get(response['next']).json()
+        responses += response['results']
+    return responses
 
-    starships1 = response1.json()
-    starships2 = response2.json()
-    starships3 = response3.json()
-    starships4 = response4.json()
-    starships = starships1['results'] + starships2['results'] + starships3['results'] + starships4['results']
-    return starships
-
-
-pprint(get_from_api())
 

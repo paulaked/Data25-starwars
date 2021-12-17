14b0914d1738c484b821bfc11cac7d3145a2f822
micha <mmcarthur@spartaglobal.com>
2021-12-17 14:06:23 +0000

    testing api url

--- starwars/app/api_data.py
+++ starwars/app/api_data.py
@@ -4,8 +4,8 @@ from pprint import pprint
 
 
 def get_api_url():
-    starwars = requests.get("https://www.swapi.tech/api/starships/")
-    total_records = str(starwars.json()['total_records'])
+    # starwars = requests.get("https://www.swapi.tech/api/starships/")
+    total_records = str(requests.get("https://www.swapi.tech/api/starships/").json()['total_records'])
     starwars = requests.get("https://www.swapi.tech/api/starships?page=1&limit=" + total_records)
 
     url_list = []
@@ -15,10 +15,33 @@ def get_api_url():
     return url_list
 
 
-def create_json():
+def create_starship_list():
     url_list = get_api_url()
-    # for i in url_list:
-    #     with open():
+
+    starship_list = []
+    for i in url_list:
+        starship_list.append((requests.get(i)).json()['result']['properties'])
+
+    return starship_list
+        # starship_name = (requests.get(i)).json()['result']['properties']['name']
+        # file_name = starship_name + ".json"
+        # with open(file_name, 'w') as jsonfile:
+        #     json.dump((requests.get(i)).json()['result']['properties'], jsonfile, sort_keys=True, indent=4,
+        #               ensure_ascii=False)
 
 
-print(create_json())
+def find_pilot_id():
+    starship_list = create_starship_list()
+    for i in starship_list:
+        if i['pilots']:
+            for x in i['pilots']:
+
+                #request pilot get name
+                #search for name in characters get ObjectId
+                #insert id into pilot
+                #put into mongo
+
+
+def return_pilot_id():
+
+find_pilot_id()

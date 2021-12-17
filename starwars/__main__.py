2e7043ca2a1c8386ff3377fef82165a4132ee56b
bigfatpanda1 <67467476+bigfatpanda1@users.noreply.github.com>
2021-12-17 10:21:50 +0000

    accessing and visualising the urls that points to infor for pilots

--- starwars/__main__.py
+++ starwars/__main__.py
@@ -4,8 +4,13 @@ import json
 if __name__ == '__main__':
     pass
 
-API_starships = requests.get('https://www.swapi.tech/api/starships/')
-print(API_starships.status_code)        #get response from api
+api_data = requests.get('https://www.swapi.tech/api/starships/')
+data = api_data.json()
+data1 = data["results"]
+print(type(data1))
+print(type(data1[1]))
+for i in data1:
+    print(i["url"])
 
 
-print(API_starships.json())
+

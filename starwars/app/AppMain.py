c594ccf4271deda8d0986490e9e3249284d02dd3
exm040 <exm040@student.bham.ac.uk>
2021-12-17 10:45:03 +0000

    reformat files & add placeholders for all functions

--- starwars/app/AppMain.py
+++ starwars/app/AppMain.py
@@ -6,17 +6,30 @@ db = client["starwars"]
 
 
 def drop_starships():
-
     # If entries in the database exist, then drop them.
     if db.starships.find({}) != "":
         db.starships.delete_many({})
     # Otherwise, proceed as normal.
 
 
-# EXTRA TESTS #
+def pull_data():
+    # Get all data from SWAPI.
+    pass
+
+
+def replace_oids():
+    # Replace pilots with list of OIDs from Characters.
+    pass
+
+
+def insert_starships():
+    # Insert starships into the database.
+    pass
+
+
+# --------------- EXTRA TESTS --------------- #
 
 drop_starships()
 testing1 = db.starships.find({})
 for i in testing1:
     print(i)
-
--- starwars/tests/main_test.py
+++ starwars/tests/main_test.py
@@ -1,8 +1,23 @@
 import pytest
-from starwars.app.DropShips import drop_starships, db
+from starwars.app.AppMain import *
 
 
+# Test that drop_starships empties the starships collection.
 def test_drop_starships():
+    drop_starships()
     assert db.starships.find({}) != ""
 
 
+# Test that data has been pulled from the API.
+def test_pull_data():
+    pull_data()
+
+
+# Test that pilots have been replaced with ObjectIDs.
+def test_replace_oids():
+    replace_oids()
+
+
+# Test that starships data has been inserted into the starships collection.
+def test_insert_starships():
+    insert_starships()

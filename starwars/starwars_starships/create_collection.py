16c9b709df87b8fc8e4a0af338c851f09e1faec6
Your Name <you@example.com>
2021-12-17 15:57:45 +0000

    collection successfully added to starwars

--- starwars/starwars_starships/create_collection.py
+++ starwars/starwars_starships/create_collection.py
@@ -3,6 +3,14 @@ import pymongo
 from upload_starships import insert_starships
 from pilot_keys import insert_pilots
 
+client = pymongo.MongoClient()
+db = client['starwars']
+
 
 def starships_collection(starships_list):
     db.create_collection("starships")
+    for starship in starships_list:
+        db.starships.insert_one(starship)
+
+
+starships_collection(insert_pilots(insert_starships("https://www.swapi.tech/api/starships/")))
--- starwars/starwars_starships/pilot_keys.py
+++ starwars/starwars_starships/pilot_keys.py
@@ -1,22 +1,25 @@
 from upload_starships import insert_starships
+import pymongo
 import requests
 import pprint as pp
+
+client = pymongo.MongoClient()
+db = client['starwars']
 
 
 def insert_pilots(starships_list):
     for ship in starships_list:
         if ship['pilots'] == []:
-            pass
+            ship['pilots'] = 'No pilots'
         else:
             pilots = ship['pilots']
             pilot_list = []
             for pilot in pilots:
                 pilot_name = requests.get(pilot).json()['result']['properties']['name']
-                pilot_info = ('db.characters.find_one({"name":"' + pilot_name +'"}, {"_id":1})' )
+                pilot_info = db.characters.find_one({"name": pilot_name}, {"_id": 1})
                 pilot_list.append(pilot_info)
             ship['pilots'] = pilot_list
 
     return starships_list
 
-
-pp.pprint(insert_pilots(insert_starships("https://www.swapi.tech/api/starships/")))
+# pp.pprint(insert_pilots(insert_starships("https://www.swapi.tech/api/starships/")))

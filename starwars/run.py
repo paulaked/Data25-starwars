8405cfff83ac73994909b23b3e75f5d96248018c
Megan Waterhouse <scout@Megans-MacBook-Pro.local>
2021-12-17 15:30:06 +0000

    extracted all separate starship URLs correctly

--- starwars/run.py
+++ starwars/run.py
@@ -2,12 +2,13 @@
 import json
 import pymongo
 import requests
+from pprint import pprint
 from app import function_list
 #
 api_address = 'https://www.swapi.tech/api/starships'
 
 web_address = api_address
-next_page_url_list = []
+next_page_url_list = [api_address]
 while True:
     page_contents = function_list.extract_data(web_address)
     next_page_url = function_list.get_all_page_urls(page_contents)
@@ -17,5 +18,8 @@ while True:
     web_address = next_page_url
 
 print(next_page_url_list)
+for url in next_page_url_list:
+    data = function_list.extract_data(url)
+    for i in data['results']:
 
-for url in 
+        pprint(i['url'])

eadb095932eaa920dd9c31c3906faa71b8b057e1
Your Name <you@example.com>
2021-12-17 11:05:59 +0000

    pilot_keys test successful

--- starwars/tests/starwars_test.py
+++ starwars/tests/starwars_test.py
@@ -1,5 +1,6 @@
 import unittest
 from upload_starships import insert_starships
+from pilot_keys import insert_pilots
 
 
 class test_upload(unittest.TestCase):
@@ -7,10 +8,12 @@ class test_upload(unittest.TestCase):
         input = insert_starships("https://www.swapi.tech/api/starships/")
         self.assertIsInstance(input, list)  # add assertion here
 
+
 class test_pilots(unittest.TestCase):
     def test_type(self):
-        input =
-        self.assertIsInstance(input, str)
+        input = insert_pilots(insert_starships("https://www.swapi.tech/api/starships/"))
+        self.assertIsInstance(input, list)
+
 
 if __name__ == '__main__':
     unittest.main()

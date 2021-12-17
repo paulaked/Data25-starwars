c007fd98cebff680a814f307205bc136a44de68b
Your Name <you@example.com>
2021-12-17 10:55:15 +0000

    pilot_key insert pilot names to starships_list

--- starwars/starwars_starships/pilot_keys.py
+++ starwars/starwars_starships/pilot_keys.py
@@ -1,35 +1,35 @@
-# from upload_starships import insert_starships
+from upload_starships import insert_starships
 import requests
 import pprint as pp
 
-ex_starship = {'MGLT': '75',
-                'cargo_capacity': '100000',
-                'consumables': '2 months',
-                'cost_in_credits': '100000',
-                'created': '2020-09-17T17:55:06.604Z',
-                'crew': '4',
-                'edited': '2020-09-17T17:55:06.604Z',
-                'hyperdrive_rating': '0.5',
-                'length': '34.37',
-                'manufacturer': 'Corellian Engineering Corporation',
-                'max_atmosphering_speed': '1050',
-                'model': 'YT-1300 light freighter',
-                'name': 'Millennium Falcon',
-                'passengers': '6',
-                'pilots': ['https://www.swapi.tech/api/people/13',
-                           'https://www.swapi.tech/api/people/14',
-                           'https://www.swapi.tech/api/people/25',
-                           'https://www.swapi.tech/api/people/31'],
-                'starship_class': 'Light freighter',
-                'url': 'https://www.swapi.tech/api/starships/10'}
+# ex_starship = {'MGLT': '75',
+#                 'cargo_capacity': '100000',
+#                 'consumables': '2 months',
+#                 'cost_in_credits': '100000',
+#                 'created': '2020-09-17T17:55:06.604Z',
+#                 'crew': '4',
+#                 'edited': '2020-09-17T17:55:06.604Z',
+#                 'hyperdrive_rating': '0.5',
+#                 'length': '34.37',
+#                 'manufacturer': 'Corellian Engineering Corporation',
+#                 'max_atmosphering_speed': '1050',
+#                 'model': 'YT-1300 light freighter',
+#                 'name': 'Millennium Falcon',
+#                 'passengers': '6',
+#                 'pilots': ['https://www.swapi.tech/api/people/13',
+#                            'https://www.swapi.tech/api/people/14',
+#                            'https://www.swapi.tech/api/people/25',
+#                            'https://www.swapi.tech/api/people/31'],
+#                 'starship_class': 'Light freighter',
+#                 'url': 'https://www.swapi.tech/api/starships/10'}
 
-pilots = ex_starship['pilots']
-pilot_list = []
-for pilot in pilots:
-    pilot_info = requests.get(pilot).json()['result']['properties']['name']
-    pilot_list.append(pilot_info)
-
-pp.pprint(pilot_list)
+# pilots = ex_starship['pilots']
+# pilot_list = []
+# for pilot in pilots:
+#     pilot_info = requests.get(pilot).json()['result']['properties']['name']
+#     pilot_list.append(pilot_info)
+#
+# pp.pprint(pilot_list)
 
 # for ship in initial_list
 #     pilots = ship['pilots']
@@ -37,3 +37,20 @@ pp.pprint(pilot_list)
 # for pilot in pilots:
 #     pilot_info = requests.get(pilot)
 
+
+def insert_pilots(starships_list):
+    for ship in starships_list:
+        if ship['pilots'] == []:
+            pass
+        else:
+            pilots = ship['pilots']
+            pilot_list = []
+            for pilot in pilots:
+                pilot_info = requests.get(pilot).json()['result']['properties']['name']
+                pilot_list.append(pilot_info)
+            ship['pilots'] = pilot_list
+
+    return starships_list
+
+
+print(insert_pilots(insert_starships("https://www.swapi.tech/api/starships/")))

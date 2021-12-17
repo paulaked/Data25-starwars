f10384f43b5d3c027ce2f21e6372bd9d061194ac
Your Name <you@example.com>
2021-12-17 11:36:40 +0000

    inserted syntax to find _id from characters

--- starwars/starwars_starships/pilot_keys.py
+++ starwars/starwars_starships/pilot_keys.py
@@ -11,11 +11,12 @@ def insert_pilots(starships_list):
             pilots = ship['pilots']
             pilot_list = []
             for pilot in pilots:
-                pilot_info = requests.get(pilot).json()['result']['properties']['name']
+                pilot_name = requests.get(pilot).json()['result']['properties']['name']
+                pilot_info = ('db.characters.find_one({"name":"' + pilot_name +'"}, {"_id":1})' )
                 pilot_list.append(pilot_info)
             ship['pilots'] = pilot_list
 
     return starships_list
 
 
-print(insert_pilots(insert_starships("https://www.swapi.tech/api/starships/")))
+pp.pprint(insert_pilots(insert_starships("https://www.swapi.tech/api/starships/")))

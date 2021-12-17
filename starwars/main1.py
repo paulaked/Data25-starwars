a6ac87fe3beff93d4dfc380a2ed8a139562fe75f
Megan Waterhouse <scout@Megans-MacBook-Pro.local>
2021-12-17 11:58:58 +0000

    restarted project, made new file for all functions

--- starwars/main1.py
+++ starwars/main1.py
@@ -5,29 +5,15 @@ import requests
 '''
 plan
 extract starship data
-write function to read and extract dictionaries about people within the startship
+write function to read and extract dictionaries about people within the starship
 output the starship data with pilots being people ID
-may not have to dump to a file
 '''
-raw_data_people = requests.get('https://www.swapi.tech/api/people')
-#pprint(raw_data_people.json())
-
-raw_data_starships = requests.get('https://www.swapi.tech/api/starships')
-pprint(raw_data_starships) # when .json() that gives output I want
-#  create new variable to dump the request output in json format
-json_data_starships = raw_data_starships.json()
-pprint(json_data_starships)
+#  create a function utilising API to get data
 
 
-#dictionary = {"starship":"tatooine"}
-with open("starships_API_with_URLs","w") as jsonfile:
-    json.dump(json_data_starships, jsonfile)
 
 
-#
-# response = requests.get('https://www.swapi.tech/api/starships')
-# print(response.status_code)
-# ship = response.json()
-# #pprint(ship)
-# for val in ship['results':'url']:
-#     print(val)
+def extract_urls(request):
+    url_list = []
+    if json_data_starships["next"] != None:
+        page_n = requests.get(json_data_starships["next"])

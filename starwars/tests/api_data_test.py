57ec75d3222e884ea08088651a3c3dade535a4cf
micha <mmcarthur@spartaglobal.com>
2021-12-18 21:15:04 +0000

    test all starship urls are 200

--- starwars/tests/api_data_test.py
+++ starwars/tests/api_data_test.py
@@ -1,2 +1,20 @@
 #check if all locations are correct type; dict string url name
 #results and properties
+import requests
+
+
+def test_for_url():
+    total_records = str(requests.get("https://www.swapi.tech/api/starships/").json()['total_records'])
+    star_wars = requests.get("https://www.swapi.tech/api/starships?page=1&limit=" + total_records)
+
+    check_url = 0
+    check = 0
+    for i in star_wars.json()['results']:
+        check += 1
+        if i.status_code == 200:
+            check_url += 1
+
+    assert check_url == check
+
+
+def

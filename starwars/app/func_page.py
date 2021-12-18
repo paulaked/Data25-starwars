e71f36f235c5f86a18573068dc9152b5b7cc1fe3
AdamMousley <AMousley@spartaglobal.com>
2021-12-18 14:01:53 +0000

    A dictionary created containing the people_Url and the pilot_Id code

--- starwars/app/func_page.py
+++ starwars/app/func_page.py
@@ -19,17 +19,17 @@ def turn_page(json_data):
 # Create a function to extract information from all pages
 
 
-def extract_url(json_data):
-    starship_url = []
-    for i in json_data["results"]:
-        starship_url.append(i["url"])
-        print(i["url"])
-    if json_data["next"] != "null":
-        raw_next_page = requests.get(json_data["next"])
-        json_next_page = raw_next_page.json()
-        for i in json_next_page["results"]:
-            print(i["url"])
-#    return starship_url
+# def extract_url(json_data):
+#     starship_url = []
+#     for i in json_data["results"]:
+#         starship_url.append(i["url"])
+#         print(i["url"])
+#     if json_data["next"] != "null":
+#         raw_next_page = requests.get(json_data["next"])
+#         json_next_page = raw_next_page.json()
+#         for i in json_next_page["results"]:
+#             print(i["url"])
+# #    return starship_url
 
 #One to first access the page
 # One to extract
--- starwars/run.py
+++ starwars/run.py
@@ -29,30 +29,6 @@ for url in page_url:
         url_list_ship.append(i["url"])
 #pprint(url_list_ship)
 
-ship_pilot_url = []
-for i in url_list_ship:
-    ship_info = func_page.api_request(i)
-    pilot_url = ship_info["result"]["properties"]["pilots"]
-    ship_pilot_url.append(pilot_url)
-ship_pilot_url = [x for x in ship_pilot_url if x]
-pprint(ship_pilot_url)
-
-
-#     if ship_info["result"]["properties"]["pilots"] != []:
-#         ship_pilot_url.append(ship_info)
-#     else:
-#         pass
-# print(ship_pilot_url)
-    #print(ship_info["result"]["properties"]["pilots"])
-
-
-
-
-
-
-
-
-
 # Code to access the people information
 api_address_people = "https://www.swapi.tech/api/people"
 web_address = api_address_people
@@ -75,6 +51,55 @@ for url in page_people_url:
     for i in data["results"]:
         url_list_people.append(i["url"])
 #pprint(url_list_people)
+
+# Code to access just the pilot url and and append them to an empty lisn. return only values that arent empty
+ship_pilot_url = []
+for i in url_list_ship:
+    ship_info = func_page.api_request(i)
+    pilot_url = ship_info["result"]["properties"]["pilots"]
+    ship_pilot_url.append(pilot_url)
+ship_pilot_url = [x for x in ship_pilot_url if x]
+#pprint(ship_pilot_url)
+
+#Code to flatten the list of urls to remove a list of lists
+pilot_urls_flat = []
+for sublist in ship_pilot_url:
+    for item in sublist:
+        pilot_urls_flat.append(item)
+#pprint(pilot_urls_flat)
+
+#Code to access the pilot_id from each url
+pilot_id_list = []
+for i in pilot_urls_flat:
+    people_info = func_page.api_request(i)
+    pilot_id = people_info["result"]["_id"]
+    pilot_id_list.append(pilot_id)
+#pprint(pilot_id_list)
+
+# Code to create a dictionary
+key_list = pilot_urls_flat
+value_list = pilot_id_list
+# zip_iterator = zip(key_list, value_list)
+# dictionary_pilot_id = dict(zip_iterator)
+new_dict = {k: v for k,v in zip(key_list,value_list)}
+print(new_dict)
+#pprint(dictionary_pilot_id)
+
+
+
+
+# Pilot id is the url for the person who pilots the ship
+# we want to replace the pilot ID with the _id from each person dictionary
+# we want to make a dictionary {["pilot ID]:["_Id]}
+# then can use panda to replace the information in each starship dictionary to show the pilot IDs instead of URLs
+
+# Extract _id for each pilot URL, then make a dictionary where key is the URL and value IS id
+
+
+
+
+
+
 
 
 

c53b1aa9a48723b5f2ee10f8069d440134953a4c
micha <mmcarthur@spartaglobal.com>
2021-12-18 21:49:31 +0000

    test for correct data in api /starships endpoint

--- starwars/tests/api_data_test.py
+++ starwars/tests/api_data_test.py
@@ -2,8 +2,21 @@
 
 import requests
 
+#   test for correct data in api /starships endpoint
+def test_all_starships_endpoint():
+    dict_key = requests.get("https://www.swapi.tech/api/starships/").json()['total_records']
+    is_true = False
+    if 'results' in dict_key:
+        if type(dict_key['results']) == list:
+            for i in dict_key['results']:
+                if 'url' in dict_key['results']:
+                    is_true = True
+
+    assert is_true is True
+
+
 #   test all individual starship urls are correct
-def test_for_url():
+def test_url():
     total_records = str(requests.get("https://www.swapi.tech/api/starships/").json()['total_records'])
     starship = requests.get("https://www.swapi.tech/api/starships?page=1&limit=" + total_records)
 
@@ -11,14 +24,14 @@ def test_for_url():
     check = 0
     for i in starship.json()['results']:
         check += 1
-        if i.status_code == 200:
+        if i['url'].status_code == 200:
             check_url += 1
 
     assert check_url == check
 
 
 #   test all starship links are starships
-def test_for_starships():
+def test_if_starships():
     first_starship = requests.get("https://www.swapi.tech/api/starships/").json()['results'][0]['url']
     is_starship = requests.get(first_starship).json()['result']['description']
 

85a2e80c942e2422de3815ecc2086d6394322913
micha <mmcarthur@spartaglobal.com>
2021-12-18 21:34:09 +0000

    test starship urls are starships

--- starwars/app/get_starship_url.py
+++ starwars/app/get_starship_url.py
@@ -6,11 +6,11 @@ def get_api_url():
     # find total number of records in API
     total_records = str(requests.get("https://www.swapi.tech/api/starships/").json()['total_records'])
     # concantenate total records to show all starships on one page
-    star_wars = requests.get("https://www.swapi.tech/api/starships?page=1&limit=" + total_records)
+    starship = requests.get("https://www.swapi.tech/api/starships?page=1&limit=" + total_records)
 
     url_list = []
     # appending the 'results' values (individual starship urls) into a list
-    for i in star_wars.json()['results']:
+    for i in starship.json()['results']:
         url_list.append(i['url'])
 
     return url_list
--- starwars/tests/api_data_test.py
+++ starwars/tests/api_data_test.py
@@ -1,18 +1,25 @@
-#check if all locations are correct type; dict string url name
-#results and properties
+# tests to check if all data from aip is correct type
+
 import requests
 
-
+#   test all individual starship urls are correct
 def test_for_url():
     total_records = str(requests.get("https://www.swapi.tech/api/starships/").json()['total_records'])
-    star_wars = requests.get("https://www.swapi.tech/api/starships?page=1&limit=" + total_records)
+    starship = requests.get("https://www.swapi.tech/api/starships?page=1&limit=" + total_records)
 
     check_url = 0
     check = 0
-    for i in star_wars.json()['results']:
+    for i in starship.json()['results']:
         check += 1
         if i.status_code == 200:
             check_url += 1
 
     assert check_url == check
 
+
+#   test all starship links are starships
+def test_for_starships():
+    first_starship = requests.get("https://www.swapi.tech/api/starships/").json()['results'][0]['url']
+    is_starship = requests.get(first_starship).json()['result']['description']
+
+    assert is_starship == "A Starship"

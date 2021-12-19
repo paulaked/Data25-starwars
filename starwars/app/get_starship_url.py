1532c469495417e84abcf491a08150f55da926a9
micha <mmcarthur@spartaglobal.com>
2021-12-19 17:26:09 +0000

    imported config manager to use SWAPI API as url

--- starwars/app/get_starship_url.py
+++ starwars/app/get_starship_url.py
@@ -1,13 +1,14 @@
 import requests
+import starwars.config_manager as config
 
 
 #   Function gets the total_records from the main starship page
 #   gets the data from new endpoint showing all records in one place
 def get_api_url():
     # find total number of records in API
-    total_records = str(requests.get("https://www.swapi.tech/api/starships/").json()['total_records'])
+    total_records = str(requests.get(config.SWAPI_URL + "/api/starships/").json()['total_records'])
     # concatenate total records to show all starships on one page
-    starships = requests.get("https://www.swapi.tech/api/starships?page=1&limit=" + total_records)
+    starships = requests.get(config.SWAPI_URL + "/api/starships?page=1&limit=" + total_records)
 
     url_list = []
     # appending the 'results' values (starship urls) to a list

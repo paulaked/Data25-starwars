116a914a38d2210c0a51b35a5e3ece287c5b49c0
micha <mmcarthur@spartaglobal.com>
2021-12-19 01:05:28 +0000

    changed variable names and added comments

--- starwars/app/return_pilot_id.py
+++ starwars/app/return_pilot_id.py
@@ -4,6 +4,8 @@ client = pymongo.MongoClient()
 db = client['starwars']
 
 
+#   takes a name argument as a string finding a match in the characters collection of the starwars database
+#   returns the matched characters id as a dictionary key, value
 def return_pilot_id(name):
     char_id = db.characters.find_one({'name': name}, {'_id': 1})
     return char_id

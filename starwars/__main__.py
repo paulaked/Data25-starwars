0a99c5a9d52a028bcf29569fcdc79e078d336446
micha <mmcarthur@spartaglobal.com>
2021-12-19 00:35:21 +0000

    added print statments and timer to main.py

--- starwars/__main__.py
+++ starwars/__main__.py
@@ -1,4 +1,8 @@
+import time
 from app.drop_create_insert_collection import drop_create_insert_collection
 
 if __name__ == '__main__':
+    start = time.time()
     drop_create_insert_collection()
+    end = time.time()
+    print(f"Sucess!!\nThe Starship collection has been created.\nElapsed time: {end - start}")

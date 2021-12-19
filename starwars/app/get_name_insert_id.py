58aa0b188ffeba7684c5055248db840712f3ea37
micha <mmcarthur@spartaglobal.com>
2021-12-19 01:05:20 +0000

    changed variable names and added comments

--- starwars/app/get_name_insert_id.py
+++ starwars/app/get_name_insert_id.py
@@ -1,14 +1,23 @@
 import requests
-from create_starship_list import create_starship_list
-from return_pilot_id import return_pilot_id
+from app.create_starship_list import create_starship_list
+from app.return_pilot_id import return_pilot_id
 
 
+#   Function iterates through pilot name from starship_data_list
+#   requests the pilots name from the api and calls function giving pilot name, assigns the returned pilot id
+#   replaces the pilot list value with the corresponding pilot id in the full starship data list
 def get_name_insert_id():
-    starship_list = create_starship_list()
-    for i in starship_list:
-        if i['pilots']:
-            for x in i['pilots']:
-                pilot_id = return_pilot_id(requests.get(x).json()['result']['properties']['name'])
-                starship_list[starship_list.index(i)]['pilots'][i['pilots'].index(x)] = pilot_id
+    # get starship data list from create_starship_list function
+    starship_data_list = create_starship_list()
+    #   iterate through each starships data
+    for ship_data in starship_data_list:
+        #   check for populated pilot lists
+        if ship_data['pilots']:
+            #   iterate though each pilots api
+            for pilot in ship_data['pilots']:
+                #   requests pilots name and call function giving pilot name and assigns the returned pilot id
+                pilot_id = return_pilot_id(requests.get(pilot).json()['result']['properties']['name'])
+                #   replaces the pilot list value with the pilot id in the full starship data list
+                starship_data_list[starship_data_list.index(ship_data)]['pilots'][ship_data['pilots'].index(pilot)] = pilot_id
 
-    return starship_list
+    return starship_data_list

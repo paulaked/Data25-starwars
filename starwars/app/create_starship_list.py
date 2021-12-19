f6983bb19f873ab5ded490acf98d9efbf0d3ec19
micha <mmcarthur@spartaglobal.com>
2021-12-19 00:39:19 +0000

    changed variable names and added comments

--- starwars/app/create_starship_list.py
+++ starwars/app/create_starship_list.py
@@ -1,14 +1,16 @@
 import requests
-from get_starship_url import get_api_url
+from app.get_starship_url import get_api_url
 
 
+#   Function creates a list of the properties data from each starship api
+#   takes the url list and iterates through
 def create_starship_list():
     # get url list from get_api_url function
     url_list = get_api_url()
 
-    starship_list = []
+    starship_data_list = []
     # appending the starship properties values from url list into a list
-    for i in url_list:
-        starship_list.append((requests.get(i)).json()['result']['properties'])
+    for url in url_list:
+        starship_data_list.append((requests.get(url)).json()['result']['properties'])
 
-    return starship_list
+    return starship_data_list

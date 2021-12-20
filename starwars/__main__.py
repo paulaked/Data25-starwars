70d258c7d8219c60ba416fe43a2dbb09ef831953
micha <mmcarthur@spartaglobal.com>
2021-12-20 14:59:42 +0000

    changed time.time to perf_counter

--- starwars/__main__.py
+++ starwars/__main__.py
@@ -2,9 +2,9 @@ import time
 from app.drop_create_insert_collection import drop_create_insert_collection
 
 if __name__ == '__main__':
-    start = time.time()
+    start = time.perf_counter()
     drop_create_insert_collection()
-    end = time.time()
+    end = time.perf_counter()
     print(f"Success!!\n"
           f"The Starship collection has been created.\n"
           f"Elapsed time: {round(float(end - start)/60, 1)} minutes")

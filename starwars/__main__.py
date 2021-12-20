d03e4c98b2dccf871f92691715d4d85dd960017f
micha <mmcarthur@spartaglobal.com>
2021-12-20 15:03:23 +0000

    added main() function to __main__.py

--- starwars/__main__.py
+++ starwars/__main__.py
@@ -1,10 +1,15 @@
 import time
 from app.drop_create_insert_collection import drop_create_insert_collection
 
-if __name__ == '__main__':
+
+def main():
     start = time.perf_counter()
     drop_create_insert_collection()
     end = time.perf_counter()
     print(f"Success!!\n"
           f"The Starship collection has been created.\n"
           f"Elapsed time: {round(float(end - start)/60, 1)} minutes")
+
+
+if __name__ == '__main__':
+    main()

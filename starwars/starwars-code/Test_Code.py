2dc9c373340c586c35675bfa76ad532727774b9c
Jenieka <jenieka@Jeniekas-MacBook-Pro.local>
2021-12-21 13:18:05 +0000

    collecting matching object ids

--- starwars/starwars-code/Test_Code.py
+++ starwars/starwars-code/Test_Code.py
@@ -5,7 +5,6 @@ from pprint import pprint
 client = pymongo.MongoClient()
 db = client['starwars']
 
-
 starship_request1 = requests.get("https://www.swapi.tech/api/starships")
 starship_request2 = requests.get('https://www.swapi.tech/api/starships?page=2&limit=10')
 starship_request3 = requests.get('https://www.swapi.tech/api/starships?page=3&limit=10')
@@ -18,10 +17,16 @@ starships4 = starship_request4.json()
 starshipsjsons = [starships1, starships2, starships3, starships4]
 # pprint(starshipsjsons)
 
+# VARIABLES
+starships_names = []
+starships_url = []
+starships_info = []
+pilot_urls = []
+pilot_info = []
+pilot_names = []
+pilot_url2 = []
 
 def collecting_starships_and_pilots():
-    starships_names = []
-    starships_url = []
     for x in range(0, 4):
         for i in starshipsjsons[x]['results']:
             # print(i)
@@ -29,19 +34,16 @@ def collecting_starships_and_pilots():
             starships_url.append(i['url'])
     # pprint(starships_url)
 
-    starships_info = []
     for i in starships_url:
         starships_info.append(requests.get(i).json())
     # pprint(starships_info)
 
-    pilot_urls = []
     for i in starships_info:
         pilot_urls.append(i.get('result').get('properties').get('pilots'))
     # pprint(pilot_urls)
     pilot_url2 = [x for x in pilot_urls if x]
     # pprint(pilot_url2)
-    pilot_info = []
-    pilot_names = []
+
     for lists in pilot_url2:
         for elements in lists:
             pilot_info.append(requests.get(elements).json())
@@ -50,6 +52,9 @@ def collecting_starships_and_pilots():
         pilot_names.append(i.get('result').get('properties').get('name'))
     # pprint(pilot_names)
 
+
+def making_pilot_collection():
+    collecting_starships_and_pilots()
     db.create_collection("pilots")
     for i in pilot_names:
         db.pilots.insert_one({
@@ -64,11 +69,19 @@ def collecting_starships_and_pilots():
              "as": "matched_name"
         }
     }])
-    for x in db.pilots.find({}):
-        print(x)
+    # for x in db.pilots.find({}):
+    #     print(x)
 
 
+def making_dictionary():
+    making_pilot_collection()
+    matching_ids = []
+    for i in pilot_names:
+        matching_ids.append(db.pilots.find_one({"name": i}, {"_id":0, "pilot_id._id": 1}))
+    pprint(matching_ids)
+    # pilot_matchingid_dict = dict(zip(pilot_urls,matching_ids))
+    # pprint(pilot_matchingid_dict)
 
 
-collecting_starships_and_pilots()
+making_dictionary()
 

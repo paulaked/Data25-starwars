1ec27ceadc1041bcaab5d804697bbca7ff71bb34
Bart Apana <BApanasionok@spartaglobal.com>
2021-12-22 07:39:44 +0000

    Refactored StarshipsUrls class into ApiRequest and StarshipUrls classes, refactored functions to pass tests

--- starwars/app/app.py
+++ starwars/app/app.py
@@ -1,29 +1,40 @@
 import requests
 
 
-class StarshipUrls:
+class ApiRequest:
 
-    def __init__(self):
-        pass
-
-    def get_request(self, api_address):
+    def __init__(self, api_address):
         self.api_address = api_address
+
+
+    def get_request(self):
         response = requests.get(self.api_address)
         return response
 
     def make_json(self, response):
         self.response = response
-        response_json = self.response.json()
+        response_json = response.json()
         return response_json
 
-    def collect_urls(self, response_json, api_address):
-        self.response_json = response_json
-        self.api_address = api_address
+
+class StarshipUrls(ApiRequest):
+    def __init__(self, response_json):
+        response_json.self = response_json
+        super().__init__(self)
+
+
+    def collect_urls(self):
+        data = super().make_json(super().response)
         starship_urls = []
-        for page in range(1, response_json["total_pages"] + 1):
-            address = api_address + "?page=" + str(page) + "&limit=10"
-            starships = StarshipUrls.get_request(StarshipUrls(), address)
-            starships = StarshipUrls.make_json(StarshipUrls(), starships)
+        for page in range(1, data["total_pages"] + 1):
+            address = super().api_address + "?page=" + str(page) + "&limit=10"
+            starships = super(address).get_request()
+            starships = super().make_json(starships)
             for dict in starships["results"]:
                 starship_urls.append(dict["url"])
         return starship_urls
+
+# class CollectPilotNames(StarshipUrls):
+#     def __init__(self, starship_urls):
+#         CollectPilotNames.__init__(self, starship_urls)
+#
--- starwars/tests/test.py
+++ starwars/tests/test.py
@@ -1,22 +1,25 @@
 import unittest
-from starwars.app.app import StarshipUrls as s
+from starwars.app.app import ApiRequest as ar
+from starwars.app.app import StarshipUrls as su
 
-class StarshipUrlsTest(unittest.TestCase):
+
+
+class TestApiRequest(unittest.TestCase):
 
     def test_status_code(self):
         address = "https://www.swapi.tech/api/starships"
-        status_code = s.get_request(s(), address).status_code
+        status_code = ar.get_request(ar(address)).status_code
         self.assertEqual(status_code, 200)
 
     def test_type_dict(self):
         address = "https://www.swapi.tech/api/starships"
-        data = s.make_json(s(), s.get_request(s(), address))
+        data = ar.make_json(ar(address), ar.get_request(ar(address)))
         self.assertIsInstance(data, dict)
 
+
     def test_total_ship_urls(self):
-        address = "https://www.swapi.tech/api/starships"
-        data = s.make_json(s(), s.get_request(s(), address))
-        list_length = len(s.collect_urls(s(), data, address))
+        data = ar.make_json(self.a_a, a.get_request(self.a_a))
+        list_length = len(s.collect_urls(self.a_a, data))
         total_records = data["total_records"]
         self.assertEqual(list_length, total_records)
 

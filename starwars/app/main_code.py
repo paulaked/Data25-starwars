06d6ac69a93615f81a38f2dc0a825cfa64bc1a1c
EJMathew <emathew@spartaglobal.com>
2021-12-23 13:27:31 +0000

    Change pilot urls from the starship_id list to the pilot name

--- starwars/app/main_code.py
+++ starwars/app/main_code.py
@@ -10,6 +10,7 @@ response2 = requests.get(response1['next']).json()
 response3 = requests.get(response2['next']).json()
 response4 = requests.get(response3['next']).json()
 
+
 # Collect only starship info from each page into a list
 def collect_results():
     starship_id = []
@@ -28,6 +29,7 @@ def collect_results():
 
 # pprint(collect_results())
 
+
 # Collect all starship urls into a list
 def collect_urls():
     starship_urls =[]
@@ -35,7 +37,21 @@ def collect_urls():
         starship_urls.append(i['url'])
     return starship_urls
 
-pprint(collect_urls())
+# pprint(collect_urls())
+
+# Change pilot urls from the starship list to pilot name
+def change_pilot_urls_to_name():
+    starships_updated =[]
+    for i in collect_results():
+        pilot_names = []
+        for j in i['pilots']:
+            pilot_names.append(requests.get(j).json()['name'])
+        i.update({'pilots': pilot_names})
+        starships_updated.append(i)
+    return  starships_updated
+
+pprint(change_pilot_urls_to_name())
+
 
 
 # pprint(response1)

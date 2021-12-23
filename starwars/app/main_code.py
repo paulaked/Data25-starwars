633368f5a570bfb1b375c8492ebf492645ace910
EJMathew <emathew@spartaglobal.com>
2021-12-23 12:40:32 +0000

    Retrieve starwars api

--- starwars/app/main_code.py
+++ starwars/app/main_code.py
@@ -2,3 +2,14 @@ import requests
 import json
 from pprint import pprint
 
+# Retrieve starwars api data from swapi
+# Convert api to json format
+response1 = requests.get('https://swapi.dev/api/starships/').json()
+response2 = requests.get(response1['next']).json()
+response3 = requests.get(response2['next']).json()
+response4 = requests.get(response3['next']).json()
+
+pprint(response1)
+pprint(response1)
+pprint(response1)
+pprint(response1)

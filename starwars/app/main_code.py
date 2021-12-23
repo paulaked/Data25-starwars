cc56e04541d00d032e83a5de4481050093eea342
EJMathew <emathew@spartaglobal.com>
2021-12-23 12:58:14 +0000

    Gather starship info into a list

--- starwars/app/main_code.py
+++ starwars/app/main_code.py
@@ -10,7 +10,27 @@ response2 = requests.get(response1['next']).json()
 response3 = requests.get(response2['next']).json()
 response4 = requests.get(response3['next']).json()
 
-pprint(response1)
-pprint(response2)
-pprint(response3)
-pprint(response4)
+# Collect only starship info from each page into a list
+def collect_results():
+    starship_id = []
+    for i in response1['results']:
+        starship_id.append(i)
+
+    for i in response2['results']:
+        starship_id.append(i)
+
+    for i in response3['results']:
+        starship_id.append(i)
+
+    for i in response4['results']:
+        starship_id.append(i)
+    return starship_id
+
+pprint(collect_results())
+
+
+
+# pprint(response1)
+# pprint(response2)
+# pprint(response3)
+# pprint(response4)

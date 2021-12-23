4d7806c4f586a397cf0c354a10fe8dcd5fb259d0
EJMathew <emathew@spartaglobal.com>
2021-12-23 17:07:12 +0000

    Create starwars database and collections using pymongo module

--- starwars/app/main_code.py
+++ starwars/app/main_code.py
@@ -13,51 +13,51 @@ response4 = requests.get(response3['next']).json()
 
 
 # Collect only starship info from each page into a list
-def collect_results():
-    starship_id = []
-    for i in response1['results']:
-        starship_id.append(i)
-
-    for i in response2['results']:
-        starship_id.append(i)
-
-    for i in response3['results']:
-        starship_id.append(i)
-
-    for i in response4['results']:
-        starship_id.append(i)
-    return starship_id
-
-# pprint(collect_results())
-
-
-# Collect all starship urls into a list
-def collect_urls():
-    starship_urls =[]
-    for i in collect_results():
-        starship_urls.append(i['url'])
-    return starship_urls
-
-# pprint(collect_urls())
-
-# Change pilot urls from the starship list to pilot name
-def change_pilot_urls_to_name():
-    starships_updated =[]
-    for i in collect_results():
-        pilot_names = []
-        for j in i['pilots']:
-            pilot_names.append(requests.get(j).json()['name'])
-        i.update({'pilots': pilot_names})
-        starships_updated.append(i)
-    return  starships_updated
+# def collect_results():
+#     starship_id = []
+#     for i in response1['results']:
+#         starship_id.append(i)
+#
+#     for i in response2['results']:
+#         starship_id.append(i)
+#
+#     for i in response3['results']:
+#         starship_id.append(i)
+#
+#     for i in response4['results']:
+#         starship_id.append(i)
+#     return starship_id
+#
+# # pprint(collect_results())
+#
+#
+# # Collect all starship urls into a list
+# def collect_urls():
+#     starship_urls =[]
+#     for i in collect_results():
+#         starship_urls.append(i['url'])
+#     return starship_urls
+#
+# # pprint(collect_urls())
+#
+# # Change pilot urls from the starship list to pilot name
+# def change_pilot_urls_to_name():
+#     starships_updated =[]
+#     for i in collect_results():
+#         pilot_names = []
+#         for j in i['pilots']:
+#             pilot_names.append(requests.get(j).json()['name'])
+#         i.update({'pilots': pilot_names})
+#         starships_updated.append(i)
+#     return  starships_updated
 
 # pprint(change_pilot_urls_to_name())
 
 
-# client = pymongo.MongoClient()
-# db = client['star_wars_2']
-# sw_starships = db['starship_info']
-# sw_characters = db['character_info']
+client = pymongo.MongoClient()
+db = client['star_wars_2']
+sw_starships = db['starship_info']
+sw_characters = db['character_info']
 
 # Create directory in locally/repository manually
 for i in change_pilot_urls_to_name():

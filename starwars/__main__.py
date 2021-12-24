b6189959c6f496896a0a078354c195a4c03e301f
bigfatpanda1 <67467476+bigfatpanda1@users.noreply.github.com>
2021-12-24 21:05:28 +0000

    created function file and connected to mongodb

--- starwars/__main__.py
+++ starwars/__main__.py
@@ -1,16 +1,2 @@
-import requests
-import json
-
 if __name__ == '__main__':
     pass
-
-api_data = requests.get('https://www.swapi.tech/api/starships/')
-data = api_data.json()
-data1 = data["results"]
-print(type(data1))
-print(type(data1[1]))
-for i in data1:
-    print(i["url"])
-
-
-

8e9e8eb75a8433b1143f00ac2c0acf37c5098294
EJMathew <emathew@spartaglobal.com>
2021-12-25 00:20:12 +0000

    Test individual functions from main code

--- starwars/tests/test_code.py
+++ starwars/tests/test_code.py
@@ -1,1 +1,21 @@
+from app.main_code import *
+
+def test_collect_results():
+    assert any(collect_results())
+
+def test_collect_urls():
+    assert any(collect_urls())
+
+def test_change_pilot_urls_to_name():
+    assert any(change_pilot_urls_to_name())
+
+def test_upload_collection():
+    for i in sw_starships.find():
+        assert type(i) is dict
+
+def test_pilot_id():
+    for i in sw_starships.find():
+        for j in i['pilots']:
+            assert type(j) is dict
+
 

f40c38af5961f168d718a88bf7f1ba0102a1b50f
EJMathew <emathew@spartaglobal.com>
2021-12-25 00:26:35 +0000

    Comment each test success

--- starwars/tests/test_code.py
+++ starwars/tests/test_code.py
@@ -1,25 +1,30 @@
 from app.main_code import *
 
+# Test passed - data in list
 def test_collect_results():
     assert any(collect_results())
 
+
+# Test passed - urls in list
 def test_collect_urls():
     assert any(collect_urls())
 
+
+# Test passed - pilot name in list
 def test_change_pilot_urls_to_name():
     assert any(change_pilot_urls_to_name())
 
+
+# Test passed - collection data is a dictionary
 def test_upload_collection():
     for i in sw_starships.find():
         assert type(i) is dict
 
+
+# Test passed - Object in pilots is a dictionary
 def test_pilot_id():
     for i in sw_starships.find():
         for j in i['pilots']:
             assert type(j) is dict
 
-# print(test_collect_results())
-# print(test_collect_urls())
-# print(test_change_pilot_urls_to_name())
-# print(test_upload_collection())
-# print(test_pilot_id())
+

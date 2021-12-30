c01edaa4d7db55683b0dda794cc054f8e4dcc7d2
MattWoodburn <mwoodburn@spartaglobal.com>
2021-12-30 16:37:16 +0000

    added tests, general formatting

--- starwars/tests/tests_draft.py
+++ starwars/tests/tests_draft.py
@@ -1,5 +1,6 @@
 from starships_functions import *
 import pprint
+import pytest
 
 # placeholder comment
 
@@ -10,4 +11,28 @@ def test_api_response():
     check = requests.get("https://www.swapi.tech/api/starships/")
     assert check.status_code == 200
 
-# pprint.pprint(test_api_response())
+
+
+# Test 2: Check to see whether the get_relevant_info() function has actually returned data:
+
+def test_relevant_info_existence():
+    test_data = get_relevant_info()
+    assert any(test_data)
+
+
+# Test 3: Check to see whether URLs in [Pilots] have been replaced with character object IDs - done by investigating the "A-wing" entry:
+
+def test_objectids_in_data():
+    test_data = get_pilots_in_list()
+    flag = False
+    for starship in test_data:
+        if starship['name'] == "A-wing" and "ObjectID" in test_data['Pilots']:
+            flag = True
+    assert flag
+
+
+# Test 4: Check to see whether starships data has made it to the MongoDB database
+
+def test_data_in_db():
+    add_collection_to_database()
+    assert any(db.starships.find({}))

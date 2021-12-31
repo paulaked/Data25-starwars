2afb3fda54a138c048610389bc0d117ec7ba7b29
Zoe <zmoakhar@spartaglobal.com>
2021-12-31 14:10:17 +0000

    the test file added

---
+++ starwars/app/project_test.py
@@ -0,0 +1,10 @@
+from mini_project import *
+
+def test_the_source():
+    assert extract_data(web_address) == dict(extract_data(web_address))
+
+def test_the_page_urls():
+    assert get_url_for_all_pages() == list(get_url_for_all_pages())
+
+def test_the_pilots_id():
+    assert get_and_replace_pilots_id() == list(get_and_replace_pilots_id())
